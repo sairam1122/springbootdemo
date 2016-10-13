de662cf368a2e9a8fed5802372ec52b54ef21622
sai <srisairamk@newtglobal.chn.local>
2016-10-13 12:41:35 +0530

    demo1

--- src/main/java/com/example/Demo0Application.java
+++ src/main/java/com/example/Demo0Application.java
@@ -25,8 +25,6 @@ public class Demo0Application extends WebMvcConfigurerAdapter{
 	
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
-
-		
 		registry.addViewController("/welcome").setViewName("welcome");
         registry.addViewController("/addProduct").setViewName("AddProduct");
         registry.addViewController("/editProduct").setViewName("edit");
